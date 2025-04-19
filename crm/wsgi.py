286ab504836fb6d0125cfdbc8166211b8f7aad67
zakeetahawi <zakee.tahawi@elkhawaga.com>
2025-04-19 17:17:59 +0200

    Remove Mangum, use standard Django WSGI for Railway

--- crm/wsgi.py
+++ crm/wsgi.py
@@ -1,11 +1,7 @@
+print("WSGI loaded")
 import os
 from django.core.wsgi import get_wsgi_application
-from mangum import Mangum
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm.settings')
 
-# Get the Django WSGI application we want to use
 application = get_wsgi_application()
-
-# Create a Mangum adapter for ASGI/WSGI support
-handler = Mangum(application, lifespan="off")

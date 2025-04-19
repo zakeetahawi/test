74436cfa23122cad70a7aaecc5f46b704b11e643
zakeetahawi <zakee.tahawi@elkhawaga.com>
2025-04-19 18:36:29 +0200

    work

--- crm/settings.py
+++ crm/settings.py
@@ -14,11 +14,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '192.168.2.52',
-]
+import os
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 INSTALLED_APPS = [
